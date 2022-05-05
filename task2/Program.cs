232ce4cdfe14be57acbcb8b82eb352d806a34644
diplg <dimas4354@icloud.com>
2022-05-05 22:35:31 +0300

    задание на проверку являются ли триточки треугольником

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+Console.WriteLine(" Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A < B + C & B < A + C & C < A + B)
+Console.WriteLine("Является треугольником!");
+else
+Console.WriteLine("Не является трекгольником!");
