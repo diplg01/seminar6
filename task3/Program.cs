40f09c47bae90b68a47419d8b15704db11a62c6e
diplg <dimas4354@icloud.com>
2022-05-05 22:53:44 +0300

    перевод числа из 10-Й в 2-Ю

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+string resalt = string.Empty;
+while (A > 0)
+{
+    resalt = A % 2 + resalt;
+    A = A / 2;
+}
+Console.WriteLine(resalt);
